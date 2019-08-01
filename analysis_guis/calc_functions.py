Statistics table fix for Rotation/UniformDrifting analysis functions (01-08-2019)

analysis_guis/calc_functions.py
@@ -1442,21 +1442,7 @@ def run_rot_lda(data, calc_para, r_filt, i_expt, i_cell, n_trial_max, d_data=Non
         ###########################################
 
         # sets the LDA solver type
-        if lda_para['solver_type'] == 'svd':
-            # case the SVD solver
-            lda = LDA()
-        elif lda_para['solver_type'] == 'lsqr':
-            # case is the LSQR solver
-            if lda_para['use_shrinkage']:
-                lda = LDA(solver='lsqr', shrinkage='auto')
-            else:
-                lda = LDA(solver='lsqr')
-        else:
-            # case is the Eigen solver
-            if lda_para['use_shrinkage']:
-                lda = LDA(solver='eigen', shrinkage='auto')
-            else:
-                lda = LDA(solver='eigen')
+        lda = setup_lda_solver(lda_para)
 
         # memory allocation
         lda_pred, c_mat = np.zeros(N, dtype=int), np.zeros((n_grp, n_grp), dtype=int)
@@ -1676,6 +1662,180 @@ def setup_kinematic_lda_sf(data, r_filt, calc_para, i_cell, n_trial_max, w_prog,
     return spd_sf_ex, _r_filt
 
 
+def run_full_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, d_data=None):
+    '''
+
+    :param data:
+    :param spd_sf:
+    :param calc_para:
+    :param r_filt:
+    :param n_trial:
+    :param w_prog:
+    :param d_data:
+    :return:
+    '''
+
+    def run_lda_predictions(w_prog, w_str0, pw_0, n_ex, lda, spd_sf, i_grp):
+        '''
+
+        :param spd_sf:
+        :param lda:
+        :return:
+        '''
+
+        # array dimensioning
+        N, n_grp = np.size(spd_sf, axis=0), i_grp[-1] + 1
+
+        # memory allocation
+        lda_pred, c_mat = np.zeros(N, dtype=int), np.zeros((n_grp, n_grp), dtype=int)
+        lda_pred_chance, c_mat_chance = np.zeros(N, dtype=int), np.zeros((n_grp, n_grp), dtype=int)
+        p_mat = np.zeros((N, n_grp), dtype=float)
+
+        ###########################################
+        ####    LDA PREDICTION CALCULATIONS    ####
+        ###########################################
+
+        # fits the LDA model and calculates the prediction for each
+        for i_pred in range(len(i_grp)):
+            # updates the progressbar (if provided)
+            if w_prog is not None:
+                w_str = '{0}, Group {1}/{2})'.format(w_str0, i_grp[i_pred] + 1, n_grp)
+                w_prog.emit(w_str, 100. * (pw_0 + (i_pred / len(i_grp)) / n_ex))
+
+            # fits the one-out-trial lda model
+            ii = np.array(range(len(i_grp))) != i_pred
+            try:
+                lda.fit(spd_sf[ii, :], i_grp[ii])
+            except:
+                e_str = 'There was an error running the LDA analysis with the current solver parameters. ' \
+                        'Either choose a different solver or alter the solver parameters before retrying'
+                return None, False, e_str
+
+            # calculates the model prediction from the remaining trial and increments the confusion matrix
+            lda_pred[i_pred] = lda.predict(spd_sf[i_pred, :].reshape(1, -1))
+            p_mat[i_pred, :] = lda.predict_proba(spd_sf[i_pred, :].reshape(1, -1))
+            c_mat[i_grp[i_pred], lda_pred[i_pred]] += 1
+
+        # returns the final values in a dictionary object
+        return {
+            'c_mat': c_mat, 'p_mat': p_mat, 'lda_pred': lda_pred,
+        }, True, None
+
+    def set_binary_mask(i_bin, n_c, n_bin):
+        '''
+
+        :param i_bin:
+        :param n_c:
+        :param n_bin:
+        :return:
+        '''
+
+        # memory allocation
+        B = np.zeros((1, n_c * n_bin), dtype=bool)
+
+        # sets the masks values
+        for i_c in range(n_c):
+            B[0, i_c * n_bin + i_bin] = True
+
+        # returns the array
+        return B
+
+    # initialisations
+    tt = r_filt['t_type']
+    lda_para = calc_para['lda_para']
+    n_c, n_ex = len(tt), len(spd_sf)
+
+    #########################
+    ####    LDA SETUP    ####
+    #########################
+
+    # memory allocation and other initialisations
+    ind_t, xi_bin = np.array(range(n_trial)), data.rotation.spd_xi
+    n_bin = np.size(xi_bin, axis=0)
+
+    # memory allocation for accuracy binary mask calculations
+    BD = np.zeros((n_bin, n_bin * n_c), dtype=int)
+    y_acc = np.nan * np.ones((n_ex, n_bin, n_c), dtype=float)
+
+    # sets up the group indices
+    i_grp0 = np.array(cf.flat_list([list(x * np.ones(n_trial, dtype=int)) for x in range(n_bin)]))
+    i_grp = np.array(cf.flat_list([list(i_c * n_bin + i_grp0) for i_c in range(n_c)]))
+
+    # sets up the binary masks for the group/direction types
+    for i_c in range(n_c):
+        for i_bin in range(n_bin):
+            BD[i_bin, (i_c * n_bin) + i_bin] = True
+
+    # sets the LDA solver type
+    lda_0 = setup_lda_solver(lda_para)
+
+    ################################
+    ####    LDA CALCULATIONS    ####
+    ################################
+
+    # loops through each of the experiments performing the lda calculations
+    for i_ex in range(n_ex):
+        # sets the progress strings (if progress bar handle is provided)
+        if w_prog is not None:
+            w_str0, pw_0 = 'Kinematic LDA (Expt {0}/{1}'.format(i_ex + 1, n_ex), i_ex / n_ex
+
+        # combines the spiking frequencies into a single array (for the current experiment)
+        spd_sf_ex = np.hstack([np.hstack([sf[:, i_bin, :].T for i_bin in range(n_bin)]) for sf in spd_sf[i_ex]]).T
+
+        # normalises the spike count array (if required)
+        if lda_para['is_norm']:
+            N = np.size(spd_sf_ex, axis=1)
+            spd_sf_mn = repmat(np.mean(spd_sf_ex, axis=1).reshape(-1, 1), 1, N)
+            spd_sf_sd = repmat(np.std(spd_sf_ex, axis=1).reshape(-1, 1), 1, N)
+            spd_sf_ex = np.divide(spd_sf_ex - spd_sf_mn, spd_sf_sd)
+
+            # any cells where the std. deviation is zero are set to zero (to remove any NaNs)
+            spd_sf_ex[spd_sf_sd == 0] = 0
+
+        # runs the LDA predictions on the current spiking frequency array
+        lda, ok, e_str = run_lda_predictions(w_prog, w_str0, pw_0, n_ex, lda_0, spd_sf_ex, i_grp)
+        if not ok:
+            # if there was an error, then exit with a false flag
+            if w_prog is not None:
+                w_prog.emit(e_str, 'LDA Analysis Error')
+            return False
+
+        # calculates the grouping accuracy values
+        c_mat = lda['c_mat'] / n_trial
+
+        # calculates the direction accuracy values (over each condition)
+        for i_c in range(n_c):
+            c_mat_sub = c_mat[(i_c * n_bin):((i_c + 1) * n_bin), :]
+            for i_bin in range(n_bin):
+                BD_sub = set_binary_mask(i_bin, n_c, n_bin)
+                y_acc[i_ex, i_bin, i_c] = np.sum(np.multiply(c_mat_sub[i_bin, :], BD_sub))
+
+    #######################################
+    ####    HOUSE-KEEPING EXERCISES    ####
+    #######################################
+
+    # sets the lda values
+    d_data.lda = 1
+    d_data.y_acc = y_acc
+
+    # sets the rotation values
+    d_data.spd_xi = data.rotation.spd_xi
+
+    # sets a copy of the lda parameters and updates the comparison conditions
+    _lda_para = dcopy(lda_para)
+    _lda_para['comp_cond'] = data.rotation.r_obj_kine.rot_filt['t_type']
+
+    # sets the solver parameters
+    set_lda_para(d_data, _lda_para, r_filt, n_trial)
+
+    # sets the phase duration/offset parameters
+    d_data.vel_bin = calc_para['vel_bin']
+    d_data.n_sample = calc_para['n_sample']
+    d_data.equal_time = calc_para['equal_time']
+
+    # returns a true value indicating success
+    return True
+
 def run_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, d_data=None):
     '''
 
@@ -1701,7 +1861,7 @@ def run_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, d_d
         :return:
         '''
 
-        # array dimensioning
+        # array dimensioning and memory allocation
         i_grp, n_grp = [], 2 * n_c
         N, n_cell = 2 * n_t * n_c, np.size(spd_sf, axis=0)
 
@@ -1735,22 +1895,8 @@ def run_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, d_d
         ####    LDA PREDICTION CALCULATIONS    ####
         ###########################################
 
-        # sets the LDA solver type
-        if lda_para['solver_type'] == 'svd':
-            # case the SVD solver
-            lda = LDA()
-        elif lda_para['solver_type'] == 'lsqr':
-            # case is the LSQR solver
-            if lda_para['use_shrinkage']:
-                lda = LDA(solver='lsqr', shrinkage='auto')
-            else:
-                lda = LDA(solver='lsqr')
-        else:
-            # case is the Eigen solver
-            if lda_para['use_shrinkage']:
-                lda = LDA(solver='eigen', shrinkage='auto')
-            else:
-                lda = LDA(solver='eigen')
+        # creates the lda solver object
+        lda = setup_lda_solver(lda_para)
 
         # memory allocation
         lda_pred, c_mat = np.zeros(N, dtype=int), np.zeros((n_grp, n_grp), dtype=int)
@@ -1818,12 +1964,9 @@ def run_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, d_d
     ################################
 
     # memory allocation and other initialisations
+    i_bin_spd = data.rotation.i_bin_spd
     ind_t, xi_bin = np.array(range(n_trial)), data.rotation.spd_xi
     n_bin = np.size(xi_bin, axis=0)
-
-    # retrieves the dependent time bin index
-    r_data = data.rotation
-    i_bin_spd = r_data.i_bin_spd
 
     # memory allocation for accuracy binary mask calculations
     BD = np.zeros((2, 2 * n_c), dtype=bool)
@@ -1869,13 +2012,9 @@ def run_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, d_d
     #######################################
 
     if d_data is not None:
-        # calculates the psychometric curves
-        y_acc_fit = calc_psychometric_curve(np.mean(y_acc, axis=0), float(calc_para['vel_bin']), n_c, r_data.i_bin_spd)
-
         # sets the lda values
         d_data.lda = 1
         d_data.y_acc = y_acc
-        d_data.y_acc_fit = y_acc_fit
         d_data.exp_name = [os.path.splitext(os.path.basename(x['expFile']))[0] for x in data.cluster]
 
         # sets the rotation values
@@ -1894,6 +2033,11 @@ def run_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, d_d
         d_data.vel_bin = calc_para['vel_bin']
         d_data.n_sample = calc_para['n_sample']
         d_data.equal_time = calc_para['equal_time']
+
+        # calculates the psychometric curves
+        y_acc_mn, d_vel, vel_mx = np.mean(y_acc, axis=0), float(calc_para['vel_bin']), 80.
+        xi_fit = np.arange(d_vel, vel_mx + 0.01, d_vel)
+        d_data.y_acc_fit, _, _, _ = calc_psychometric_curves(y_acc_mn, xi_fit, n_c, i_bin_spd)
 
         # returns a true value indicating success
         return True
@@ -2187,6 +2331,10 @@ def init_lda_para(d_data_0, d_data_f=None, d_data_def=None):
         # case is the default rotational LDA
         def_para = set_def_lda_para(d_data, ['tofs', 'tphase', 'usefull', 'yaccmn', 'yaccmx'])
 
+    elif d_data.type in ['SpdAcc']:
+        # case is the velocity comparison LDA
+        def_para = set_def_lda_para(d_data, ['vel_bin', 'n_sample', 'equal_time'])
+
     elif d_data.type in ['SpdComp']:
         # case is the velocity comparison LDA
         def_para = set_def_lda_para(d_data, ['spd_xrng', 'vel_bin', 'n_sample', 'equal_time'])
@@ -2212,7 +2360,7 @@ def set_def_para(para, p_str, def_val):
     return para[p_str] if p_str in para else def_val
 
 
-def set_lda_para(d_data, lda_para, r_filt, n_trial_max, ignore_list=[]):
+def set_lda_para(d_data, lda_para, r_filt, n_trial_max, ignore_list=None):
     '''
 
     :param d_data:
@@ -2221,6 +2369,10 @@ def set_lda_para(d_data, lda_para, r_filt, n_trial_max, ignore_list=[]):
     :param n_trial_max:
     :return:
     '''
+
+    #
+    if ignore_list is None:
+        ignore_list = []
 
     # sets the parameter to class conversion strings
     conv_str = {
@@ -2332,6 +2484,34 @@ def calc_binned_kinemetic_spike_freq(data, plot_para, calc_para, w_prog, roc_cal
             r_data.vel_sf[tt], r_data.spd_sf[tt] = dcopy(vel_f[i_filt]), dcopy(spd_f)
 
 
+def setup_lda_solver(lda_para):
+    '''
+
+    :param lda_para:
+    :return:
+    '''
+
+    # sets the LDA solver type
+    if lda_para['solver_type'] == 'svd':
+        # case the SVD solver
+        lda = LDA()
+    elif lda_para['solver_type'] == 'lsqr':
+        # case is the LSQR solver
+        if lda_para['use_shrinkage']:
+            lda = LDA(solver='lsqr', shrinkage='auto')
+        else:
+            lda = LDA(solver='lsqr')
+    else:
+        # case is the Eigen solver
+        if lda_para['use_shrinkage']:
+            lda = LDA(solver='eigen', shrinkage='auto')
+        else:
+            lda = LDA(solver='eigen')
+
+    # returns the lda solver object
+    return lda
+
+
 def get_pool_cell_counts(data, lda_para, type=0):
     '''
 
@@ -2347,7 +2527,34 @@ def get_pool_cell_counts(data, lda_para, type=0):
 
     # determine the valid cell counts for each experiment
     _, _, i_cell, _, _ = setup_lda(data, {'lda_para': lda_para}, None)
-    n_cell_tot = np.sum([sum(x) for x in i_cell])
 
     # returns the final cell count
-    return [x for x in n_cell if x <= n_cell_tot] + ([n_cell_tot] if n_cell_tot not in n_cell else [])
+    if i_cell is None:
+        return []
+    else:
+        n_cell_tot = np.sum([sum(x) for x in i_cell])
+        return [x for x in n_cell if x <= n_cell_tot] + ([n_cell_tot] if n_cell_tot not in n_cell else [])
+
+
+# def normalise_spike_freq(spd_sf_calc, N, i_ax=1):
+#     '''
+#
+#     :param spd_sf_calc:
+#     :param N:
+#     :return:
+#     '''
+#
+#     # normalises the spiking frequencies
+#     if i_ax == 0:
+#         spd_sf_mn = repmat(np.mean(spd_sf_calc, axis=0).reshape(-1, 1), N, 1)
+#         spd_sf_sd = repmat(np.std(spd_sf_calc, axis=0).reshape(-1, 1), N, 1)
+#     else:
+#         spd_sf_mn = repmat(np.mean(spd_sf_calc, axis=1).reshape(-1, 1), 1, N)
+#         spd_sf_sd = repmat(np.std(spd_sf_calc, axis=1).reshape(-1, 1), 1, N)
+#
+#     # any cells where the std. deviation is zero are set to zero (to remove any NaNs)
+#     spd_sf_calc = np.divide(spd_sf_calc - spd_sf_mn, spd_sf_sd)
+#     spd_sf_calc[spd_sf_sd == 0] = 0
+#
+#     # returns the normalised array
+#     return spd_sf_calc

analysis_guis/common_func.py
@@ -1908,8 +1908,8 @@ def calc_cell_group_types(auc_sig, stats_type):
 
 
 # calculates the table dimensions
-def add_plot_table(fig, ax, font, data, row_hdr, col_hdr, row_cols, col_cols, t_loc,
-                   cell_cols=None, n_row=1, n_col=2, pfig_sz=1.0, t_ofs=0, h_title=None, p_wid=1.0):
+def add_plot_table(fig, ax, font, data, row_hdr, col_hdr, row_cols, col_cols, t_loc, cell_cols=None,
+                   n_row=1, n_col=2, pfig_sz=1.0, t_ofs=0, h_title=None, p_wid=1.5, ax_pos_tbb=None):
     '''
 
     :param ax:
@@ -1930,6 +1930,10 @@ def add_plot_table(fig, ax, font, data, row_hdr, col_hdr, row_cols, col_cols, t_
     # sets the axis object (if the axis index was provided)
     if isinstance(ax, int):
         ax = fig.ax[ax]
+
+    # retrieves the bounding box position array (if not provided)
+    if ax_pos_tbb is None:
+        ax_pos_tbb = ax.get_tightbbox(fig.get_renderer()).bounds
 
     # objection dimensioning
     fig_wid, fig_hght = fig.width(), fig.height() * pfig_sz
@@ -1984,7 +1988,7 @@ def add_plot_table(fig, ax, font, data, row_hdr, col_hdr, row_cols, col_cols, t_
                   n_row_data * (cell_hght0 + (n_rowhdr_line + 1) * h_gap))
 
     # if the table width it too large, then rescale
-    sp_width = get_axes_tight_bbox(fig, ax, pfig_sz)[2] / ax.get_position().width
+    sp_width = get_axes_tight_bbox(fig, ax_pos_tbb, pfig_sz)[2] / ax.get_position().width
     if table_wid > sp_width:
         ptable_wid = sp_width / table_wid
         cell_wid, cell_wid_row = ptable_wid * cell_wid, ptable_wid * cell_wid_row
@@ -2003,7 +2007,7 @@ def add_plot_table(fig, ax, font, data, row_hdr, col_hdr, row_cols, col_cols, t_
     # sets the bounding box dimensions
     ax_hght_new = pfig_sz * (ax_y1 - ax_y0) / fig_hght
     ax_fig_hght = ax_hght_new * fig_hght
-    table_x0 = get_axis_scaled_xval(ax, fig, (1 - table_wid) / 2, n_col, pfig_sz)
+    table_x0 = get_axis_scaled_xval(ax, fig, ax_pos_tbb, (1 - table_wid) / 2, pfig_sz)
 
     if t_loc == 'bottom':
         table_y0 = -(table_hght + (1 + pWT) * title_hght + cell_hght0 * (1 + n_line)) / ax_fig_hght
@@ -2025,7 +2029,7 @@ def add_plot_table(fig, ax, font, data, row_hdr, col_hdr, row_cols, col_cols, t_
 
     # resets the position of the title object
     if h_title is not None:
-        x_title = get_axis_scaled_xval(ax, fig, 0.5, n_col, pfig_sz, False)
+        x_title = get_axis_scaled_xval(ax, fig, ax_pos_tbb, 0.5, pfig_sz, False)
         h_title.set_position([x_title, table_y0 + (table_hght + pWT * title_hght) / ax_fig_hght])
 
     # sets the table parameters based on whether there is a row header column
@@ -2061,7 +2065,7 @@ def add_plot_table(fig, ax, font, data, row_hdr, col_hdr, row_cols, col_cols, t_
     return [h_table, table_y0, ax_fig_hght]
 
 
-def get_axes_tight_bbox(fig, ax, pfig_sz=1.):
+def get_axes_tight_bbox(fig, ax_pos_tbb, pfig_sz=1.):
     '''
 
     :param fig:
@@ -2074,7 +2078,7 @@ def get_axes_tight_bbox(fig, ax, pfig_sz=1.):
     r_fig_pos = np.array([fig_wid, fig_hght, fig_wid, fig_hght])
 
     # returns the
-    return np.array(ax.get_tightbbox(fig.get_renderer()).bounds) / r_fig_pos
+    return ax_pos_tbb / r_fig_pos
 
 
 def get_subplot_width(fig, ax, n_col):
@@ -2087,7 +2091,7 @@ def get_subplot_width(fig, ax, n_col):
     return (t_wid_f / n_col) / ax.get_position().width
 
 
-def get_axis_scaled_xval(ax, fig, x, n_col, pfig_sz, is_scaled=True):
+def get_axis_scaled_xval(ax, fig, ax_pos_tbb, x, pfig_sz, is_scaled=True):
     '''
 
     :param ax:
@@ -2097,16 +2101,17 @@ def get_axis_scaled_xval(ax, fig, x, n_col, pfig_sz, is_scaled=True):
 
     # retrieves the axis normal/tight position vector
     ax_pos = np.array(ax.get_position().bounds)
-    ax_pos_t = get_axes_tight_bbox(fig, ax, pfig_sz)
+    ax_pos_t = get_axes_tight_bbox(fig, ax_pos_tbb, pfig_sz)
 
     # sets the column locations (for each column)
-    x_ofs = (1 - t_wid_f) / (2 * ax_pos_t[2])
-    pp = np.linspace(x_ofs + (ax_pos_t[0] - ax_pos[0]) / ax_pos[2],
-                     (1 - x_ofs) + ((ax_pos_t[0] + ax_pos_t[2]) - (ax_pos[2] + ax_pos[0])) / ax_pos[2], n_col + 1)
+    # pp = np.linspace(x_ofs + (ax_pos_t[0] - ax_pos[0]) / ax_pos[2],
+    #                  (1 - x_ofs) + ((ax_pos_t[0] + ax_pos_t[2]) - (ax_pos[2] + ax_pos[0])) / ax_pos[2], n_col + 1)
+    # i_col = int(np.floor(ax_pos[0] / (1 / n_col)) / n_col)
 
     # calculates the subplot axis left/right location
-    i_col = int(np.floor(ax_pos[0] / (1 / n_col)) / n_col)
-    sp_left, sp_right = pp[i_col], pp[i_col + 1]
+    x_ofs = (1 - t_wid_f) / (2 * ax_pos_t[2])
+    sp_left = x_ofs + (ax_pos_t[0] - ax_pos[0]) / ax_pos[2]
+    sp_right = (1 - x_ofs) + ((ax_pos_t[0] + ax_pos_t[2]) - (ax_pos[2] + ax_pos[0])) / ax_pos[2]
 
     # returns the scaled value
     if is_scaled:

analysis_guis/dialogs/rotation_filter.py
@@ -611,20 +611,29 @@ class RotationFilteredData(object):
         '''
 
         # sets the experiment indices
-        clust_ind, trial_ind = self.clust_ind, self.trial_ind
+        clust_ind, trial_ind, e_str = self.clust_ind, self.trial_ind, None
+
+        if len(clust_ind) == 0:
+            # if the cluster index is not valid, then output an error to screen
+            e_str = 'The input cluster index does not have a feasible match. Please try again with a ' \
+                    'different index or rotation analysis filter.'
+
         if self.is_single_cell:
             if i_cluster not in (clust_ind[0][0] + 1):
                 # if the cluster index is not valid, then output an error to screen
                 e_str = 'The input cluster index does not have a feasible match. Please try again with a ' \
                         'different index or rotation analysis filter.'
-                cf.show_error(e_str, 'Infeasible Cluster Indices')
-                self.is_ok = False
-                return
             else:
                 # otherwise, set the cluster index value for the given experiment
                 clust_ind = [[np.array([i_cluster-1], dtype=int)] for _ in range(self.n_filt)]
                 i_expt0 = cf.get_expt_index(self.plot_exp_name, data.cluster, cf.det_valid_rotation_expt(data))
                 self.i_expt0 = [np.array([i_expt0]) for _ in range(self.n_filt)]
+
+        # if there was an error then output a message to screen and exit the function
+        if e_str is not None:
+            cf.show_error(e_str, 'Infeasible Cluster Indices')
+            self.is_ok = False
+            return
 
         if data.cluster is None:
             s_freq = [[data._cluster[i]['sFreq'] for i in x] for x in self.i_expt0]

analysis_guis/main_analysis.py
@@ -567,6 +567,10 @@ class AnalysisGUI(QMainWindow):
                                 self.data.multi = MultiFileData()
                                 self.data.multi.set_multi_file_data(self.worker[iw].thread_job_para[0])
 
+                    if init_data:
+                        self.data.rotation.exc_rot_filt = cf.init_rotation_filter_data(False, is_empty=True)
+                        self.data.rotation.exc_ud_filt = cf.init_rotation_filter_data(True, is_empty=True)
+
                 # sets up the analysis functions and resets the current parameter fields
                 self.fcn_data.init_all_func()
 
@@ -3534,8 +3538,8 @@ class AnalysisGUI(QMainWindow):
     ####    UNIFORM DRIFT ANALYSIS FUNCTIONS    ####
     ################################################
 
-    def plot_unidrift_trial_spikes(self, rot_filt, i_cluster, plot_exp_name, plot_all_expt, plot_scope, show_pref_dir,
-                                   n_bin, plot_grid, rmv_median):
+    def plot_unidrift_trial_spikes(self, rot_filt, i_cluster, plot_exp_name, plot_all_expt, plot_scope,
+                                   n_bin, plot_grid, rmv_median, show_pref_dir):
         '''
 
         :param rot_filter:
@@ -4728,7 +4732,7 @@ class AnalysisGUI(QMainWindow):
 
         # initialisations and memory allocation
         n_grp, r_data = [2, 4, 2], self.data.rotation
-        stats_type = ['Rotation Motion Sensitivity', 'Rotation/Visual DS', 'Congruency']
+        stats_type = ['Rotation Motion Sensitivity', 'Rotation/Visual Direction Selectivity', 'Congruency']
 
         #
         if plot_type == 'Motion Sensitivity':
@@ -4795,6 +4799,7 @@ class AnalysisGUI(QMainWindow):
         # initialisations
         t_props, n_filt = np.empty(len(t_str), dtype=object), r_data.r_obj_rot_ds.n_filt
         t_data = [cf.add_rowcol_sum(n_MS).T, cf.add_rowcol_sum(n_DS).T, cf.add_rowcol_sum(n_PD)]
+        ax_pos_tbb = dcopy(ax.get_tightbbox(self.plot_fig.get_renderer()).bounds)
 
         # sets up the n-value table
         for i in range(len(t_props)):
@@ -4802,7 +4807,7 @@ class AnalysisGUI(QMainWindow):
             nT = n_grp[i]
             t_props[i] = cf.add_plot_table(self.plot_fig, ax, table_font, t_data[i].astype(int), row_hdr, col_hdr[i],
                                            cT[:n_filt] + [(0.75, 0.75, 0.75)], cT[:nT]  + [(0.75, 0.75, 0.75)],
-                                           None, n_row=1, n_col=2, h_title=h_title[i])
+                                           None, n_row=1, n_col=2, ax_pos_tbb=ax_pos_tbb)
 
             # calculates the height between the title and the top of the table
             if i == 0:
@@ -6123,6 +6128,19 @@ class AnalysisGUI(QMainWindow):
     ####    SPEED DISCRIMINATION ANALYSIS FUNCTIONS   ####
     ######################################################
 
+    def plot_speed_accuracy_lda(self, plot_grid):
+        '''
+
+        :param plot_grid:
+        :return:
+        '''
+
+        # initialisations
+        d_data = self.data.discrim.spdacc
+        n_cond = len(d_data.ttype)
+        col = cf.get_plot_col(n_cond)
+
+
     def plot_speed_comp_lda(self, show_cell_sz, show_fit, plot_type, sep_resp, plot_grid):
         '''
 
@@ -6214,7 +6232,7 @@ class AnalysisGUI(QMainWindow):
 
                 # plots the psychometric fit (if required)
                 if show_fit:
-                    ax.plot(x_nw, 100. * d_data.y_acc_fit[i_cond], c=col[i_cond], linewidth=2)
+                    ax.plot(x_nw, 100. * d_data.y_acc_fit[:, i_cond], c=col[i_cond], linewidth=2)
 
                 # creates the legend
                 ax.legend(h_plt, d_data.ttype, loc=4)
@@ -7642,15 +7660,15 @@ class AnalysisGUI(QMainWindow):
         # initialisations
         if stats_type == 'Motion Sensitivity':
             n_DS_Full = cf.add_rowcol_sum(n_DS)
-            col_hdr = ['None', 'Inhibited', 'Excited', 'Mixed']
+            col_hdr = ['None', 'Inh.', 'Exc.', 'Mixed', 'Total']
         else:
-            n_DS_Full = cf.add_rowcol_sum(n_DS).T
+            n_DS_Full = cf.add_rowcol_sum(n_DS.T)
             col_hdr = ['Sensitive', 'Insensitive', 'Total']
 
         # creates the colours (if not provided)
-        if c is None:
-            c = cf.get_plot_col(np.size(n_DS, axis=1))
-            c2 = cf.get_plot_col(4, np.size(n_DS, axis=1))
+        # if c is None:
+        n_row_d, n_col_d = np.shape(n_DS_Full)
+        c_row, c_col = cf.get_plot_col(n_row_d - 1), cf.get_plot_col(n_col_d - 1, n_row_d - 1)
 
         # creates the title text object
         t_str = '{0} N-Values'.format(stats_type)
@@ -7658,9 +7676,10 @@ class AnalysisGUI(QMainWindow):
         row_hdr = ['#{0}'.format(x + 1) for x in range(n_filt)] + ['Total']
 
         # sets up the n-value table
+        ax_pos_tbb = dcopy(ax.get_tightbbox(self.plot_fig.get_renderer()).bounds)
         t_props = cf.add_plot_table(self.plot_fig, ax, table_font, n_DS_Full.astype(int), row_hdr,
-                                      col_hdr, c + [(0.75, 0.75, 0.75)], c2[0:4]  + [(0.75, 0.75, 0.75)], None,
-                                      n_row=n_row, n_col=n_col, h_title=h_title)
+                                      col_hdr, c_row + [(0.75, 0.75, 0.75)], c_col  + [(0.75, 0.75, 0.75)], None,
+                                      n_row=n_row, n_col=n_col, h_title=h_title, ax_pos_tbb=ax_pos_tbb)
 
         # calculates the height between the title and the top of the table
         dh_title = h_title.get_position()[1] - (t_props[0]._bbox[1] + t_props[0]._bbox[3])
@@ -7703,8 +7722,10 @@ class AnalysisGUI(QMainWindow):
             row_hdr_1 = col_hdr_1 = ['#{0}'.format(str(x + 1)) for x in range(n_filt)]
 
             # sets up the n-value table
+            c_stats = cf.get_plot_col(len(row_hdr_1))
             t_props_2 = cf.add_plot_table(self.plot_fig, ax, table_font, chi_stats, row_hdr_1, col_hdr_1,
-                                          c, c, None, n_row=n_row, n_col=n_col, h_title=h_title_2)
+                                          c_stats, c_stats, None, n_row=n_row, n_col=n_col,
+                                          ax_pos_tbb=ax_pos_tbb)
 
             # resets the bottom location of the upper table
             t_props_2[0]._bbox[1] = t_props[0]._bbox[1] - (t_props[0]._bbox[3] + c_hght)
@@ -7776,6 +7797,7 @@ class AnalysisGUI(QMainWindow):
                          'Shuffled LDA',
                          'Pooled Neuron LDA',
                          'Individual Cell Accuracy Filtered LDA',
+                         'Speed LDA Accuracy',
                          'Speed LDA Comparison (Individual Experiments)',
                          'Speed LDA Comparison (Pooled Experiments)']
 
@@ -8630,8 +8652,8 @@ class AnalysisFunctions(object):
         # ====> Rotation Trial Motion/Direction Selectivity
         para = {
             # # calculation parameters
-            # 't_phase_vis': {'gtype': 'C', 'text': 'UniformDrifting Phase Duration (s)', 'def_val': t_phase, 'min_val': 0.10},
-            # 't_ofs_vis': {'gtype': 'C', 'text': 'UniformDrifting Phase Offset (s)', 'def_val': t_ofs, 'min_val': 0.00},
+            # 't_phase_vis': {'gtype': 'C', 'text': 'Visual Phase Duration (s)', 'def_val': t_phase, 'min_val': 0.10},
+            # 't_ofs_vis': {'gtype': 'C', 'text': 'Visual Phase Offset (s)', 'def_val': t_ofs, 'min_val': 0.00},
 
             # plotting parameters
             'rot_filt': {
@@ -8786,11 +8808,12 @@ class AnalysisFunctions(object):
                 'link_para': [['i_cluster', 'Whole Experiment'], ['plot_exp_name', 'Individual Cell'],
                               ['plot_all_expt', 'Individual Cell']]
             },
-            'show_pref_dir': {'type': 'B', 'text': 'Sbow Preferred Direction', 'def_val': True},
 
             'n_bin': {'text': 'Histogram Bin Count', 'def_val': 100, 'min_val': 10},
             'plot_grid': {'type': 'B', 'text': 'Show Axes Grid', 'def_val': False},
-            'rmv_median': {'type': 'B', 'text': 'Remove Baseline Median', 'def_val': True},
+
+            'rmv_median': {'type': 'B', 'text': 'Remove Baseline Median', 'def_val': True, 'is_visible': False},
+            'show_pref_dir': {'type': 'B', 'text': 'Sbow Preferred Direction', 'def_val': False, 'is_visible': False},
         }
         self.add_func(type='UniformDrift Analysis',
                       name='UniformDrift Trial Spiking Rates',
@@ -9316,10 +9339,10 @@ class AnalysisFunctions(object):
                 'def_val': grp_stype[0], 'link_para': ['n_boot', ['Delong', 'Wilcoxon Paired Test']]
             },
             't_phase_vis': {
-                'gtype': 'C', 'text': 'UniformDrifting Phase Duration (s)', 'def_val': t_phase, 'min_val': 0.10
+                'gtype': 'C', 'text': 'Visual Phase Duration (s)', 'def_val': t_phase, 'min_val': 0.10
             },
             't_ofs_vis': {
-                'gtype': 'C', 'text': 'UniformDrifting Phase Offset (s)', 'def_val': t_ofs, 'min_val': 0.00
+                'gtype': 'C', 'text': 'Visual Phase Offset (s)', 'def_val': t_ofs, 'min_val': 0.00
             },
             'use_full_vis': {
                 'gtype': 'C', 'type': 'B', 'text': 'Use Full Visual Phase', 'def_val': False,
@@ -9701,6 +9724,7 @@ class AnalysisFunctions(object):
         ######################################
 
         # velocity LDA parameters
+        spdacc_lda_para, spdacc_def_para = init_lda_para(data.discrim, 'spdacc', SubDiscriminationData('SpdAcc'))
         spdc_lda_para, spdc_def_para = init_lda_para(data.discrim, 'spdc', SubDiscriminationData('SpdComp'))
         spdcp_lda_para, spdcp_def_para = init_lda_para(data.discrim, 'spdcp', SubDiscriminationData('SpdCompPool'))
 
@@ -9710,6 +9734,44 @@ class AnalysisFunctions(object):
 
         # determines the cell count checklist values
         n_cell_list = [str(x) for x in cfcn.get_pool_cell_counts(data, spdcp_lda_para)]
+
+        # ====> Speed LDA Accuracy
+        para = {
+            # calculation parameters
+            'lda_para': {
+                'gtype': 'C', 'type': 'Sp', 'text': 'LDA Solver Parameters', 'para_gui': LDASolverPara,
+                'def_val': spdacc_lda_para
+            },
+
+            'vel_bin': {
+                'gtype': 'C', 'type': 'L', 'text': 'Speed Bin Size (deg/sec)', 'list': roc_vel_bin,
+                'def_val': cfcn.set_def_para(spdacc_def_para, 'vel_bin', '5'),
+                'para_reset': [['spd_x_rng', self.reset_spd_rng]]
+            },
+            'n_sample': {
+                'gtype': 'C', 'text': 'Equal Timebin Resampling Count',
+                'def_val': cfcn.set_def_para(spdacc_def_para, 'n_sample', 100)
+            },
+            'equal_time': {
+                'gtype': 'C', 'type': 'B', 'text': 'Use Equal Timebins',
+                'def_val': cfcn.set_def_para(spdacc_def_para, 'equal_time', False),
+                'link_para': ['n_sample', False]
+            },
+
+            # invisible parameters
+            'freq_type': {
+                'gtype': 'C', 'type': 'L', 'text': 'Spike Frequency Type', 'list': ['All'], 'def_val': 'All',
+                'is_visible': False
+            },
+            'pn_calc': {'gtype': 'C', 'text': 'Use Pos/Neg', 'def_val': False, 'is_visible': False},
+
+            # plotting parameters
+            'plot_grid': {'type': 'B', 'text': 'Show Axes Grid', 'def_val': False},
+        }
+        self.add_func(type='Kinematic Discrimination Analysis',
+                      name='Speed LDA Accuracy',
+                      func='plot_speed_accuracy_lda',
+                      para=para)
 
         # ====> Speed LDA Comparison (Individual Experiments)
         para = {
@@ -10980,6 +11042,8 @@ class ClassifyData(object):
     def __init__(self):
         # initialisation
         self.is_set = False
+        self.class_set = False
+        self.action_set = False
 
     def init_classify_fields(self, expt_name, clust_id):
         '''
@@ -10991,8 +11055,6 @@ class ClassifyData(object):
 
         # initialisation
         self.is_set = False
-        self.class_set = False
-        self.action_set = False
 
         # memory allocation
         n_expt = len(expt_name)
@@ -11233,6 +11295,7 @@ class DiscriminationData(object):
         self.filt = SubDiscriminationData('IndivFilt')
 
         # kinematic discrimination analysis
+        self.spdacc = SubDiscriminationData('SpdAcc')
         self.spdc = SubDiscriminationData('SpdComp')
         self.spdcp = SubDiscriminationData('SpdCompPool')
 
@@ -11293,22 +11356,24 @@ class SubDiscriminationData(object):
             self.xi_phs = None
             self.xi_ofs = None
 
-        elif type in ['SpdComp', 'SpdCompPool']:
+        elif type in ['SpdAcc', 'SpdComp', 'SpdCompPool']:
             # case is the speed comparison LDA
             self.spd_xi = None
-            self.y_acc_fit = None
-            self.p_acc = None
-            self.p_acc_lo = None
-            self.p_acc_hi = None
             self.i_bin_spd = -1
-            self.spd_xrng = -1
             self.vel_bin = -1
             self.n_sample = -1
             self.equal_time = -1
 
-            if type == 'SpdCompPool':
+            if type in ['SpdComp', 'SpdCompPool']:
+                self.y_acc_fit = None
+                self.spd_xrng = -1
+
+            if type in ['SpdCompPool']:
                 # case is the pooled speed comparison LDA
                 self.n_cell = None
+                self.p_acc = None
+                self.p_acc_lo = None
+                self.p_acc_hi = None
                 self.nshuffle = -1
 
 class MultiFileData(object):