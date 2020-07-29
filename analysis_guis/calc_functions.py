Fixed minor errors with rotation analysis filtering

analysis_guis/calc_functions.py
@@ -1314,7 +1314,8 @@ def calc_noise_correl(d_data, n_sp):
                 x, y = n_sp_pair[:, 0, :].flatten(), n_sp_pair[:, 1, :].flatten()
 
                 # calculates the x/y z-scored values
-                x_z, y_z = (x - np.mean(x)) / np.std(x), (y - np.mean(y)) / np.std(y)
+                x_z = (x - np.nanmean(x)) / np.nanstd(x) if np.any(x > 0) else x
+                y_z = (y - np.nanmean(y)) / np.nanstd(y) if np.any(y > 0) else y
                 is_acc = np.logical_and(np.abs(x_z) < z_tol, np.abs(y_z) < z_tol)
 
                 # calculates the pair-wise pearson correlations
@@ -2585,11 +2586,6 @@ def run_kinematic_lda(data, spd_sf, calc_para, r_filt, n_trial, w_prog=None, w_s
         d_data.n_sample = calc_para['n_sample']
         d_data.equal_time = calc_para['equal_time']
 
-        # calculates the psychometric curves
-        y_acc_mn, d_vel, vel_mx = np.mean(y_acc, axis=0), float(calc_para['vel_bin']), 80.
-        xi_fit = np.arange(d_vel, vel_mx + 0.01, d_vel)
-        d_data.y_acc_fit, _, _, _ = calc_psychometric_curves(y_acc_mn, xi_fit, n_c, i_bin_spd)
-
         # returns a true value indicating success
         return True
     else:
@@ -2859,7 +2855,6 @@ def calc_all_psychometric_curves(d_data, d_vel, use_all=True):
     else:
         i_fit = np.arange(d_data.i_bin_spd + 1, n_xi).astype(int)
 
-
     # calculates the psychometric fits for each condition trial type
     for i_tt in range(n_tt):
         # sets the mean accuracy values (across all cell counts)
@@ -2874,7 +2869,7 @@ def calc_all_psychometric_curves(d_data, d_vel, use_all=True):
 
         # calculates/sets the psychometric fit values
         y_acc_fit[:, :, i_tt], p_acc[i_tt], p_acc_lo[i_tt], p_acc_hi[i_tt] = \
-                            calc_psychometric_curves(y_acc_mn, xi_fit, nC, i_fit, i_bin_spd)
+                                        calc_psychometric_curves(y_acc_mn, xi_fit, nC, i_fit, i_bin_spd)
 
     # updates the class fields
     d_data.y_acc_fit, d_data.p_acc, d_data.p_acc_lo, d_data.p_acc_hi = y_acc_fit, p_acc, p_acc_lo, p_acc_hi

analysis_guis/dialogs/rotation_filter.py
@@ -673,12 +673,8 @@ class RotationFilteredData(object):
         '''
 
         # sets the experiment indices
+        is_rot = cf.det_valid_rotation_expt(data)
         clust_ind, trial_ind, e_str = self.clust_ind, self.trial_ind, None
-
-        if self.use_raw:
-            cluster = data._cluster
-        else:
-            cluster = data.cluster
 
         if len(clust_ind) == 0:
             # if the cluster index is not valid, then output an error to screen
@@ -692,11 +688,15 @@ class RotationFilteredData(object):
                 e_str = 'There are no valid cells for this experiment. Retry again with another experiment.'
             else:
                 # otherwise, deteremine the index of the current experiment
-                i_expt0 = cf.get_expt_index(self.plot_exp_name, cluster, cf.det_valid_rotation_expt(data))
+                i_expt0 = cf.get_expt_index(self.plot_exp_name, data._cluster, np.ones(len(data._cluster)))
                 self.i_expt0 = [np.array([i_expt0]) for _ in range(self.n_filt)]
 
                 # sets the index values for the given experiment
-                i_cluster = data.cluster[i_expt0]['clustID'].index(int(cell_id[cell_id.index('#') + 1:]))
+                if self.use_raw:
+                    i_cluster = data._cluster[i_expt0]['clustID'].index(int(cell_id[cell_id.index('#') + 1:]))
+                else:
+                    i_cluster = data.cluster[i_expt0]['clustID'].index(int(cell_id[cell_id.index('#') + 1:]))
+
                 clust_ind = [[np.array([i_cluster], dtype=int)] for _ in range(self.n_filt)]
 
         # if there was an error then output a message to screen and exit the function

analysis_guis/main_analysis.py
@@ -3158,6 +3158,11 @@ class AnalysisGUI(QMainWindow):
             cf.show_error(e_str, 'Infeasible Cluster Indices')
             self.calc_ok = False
             return
+        elif len(i_plot) == 0:
+            m_str = 'There are no classification matches for the currently selected experiment.'
+            cf.show_error(m_str, 'No Match Types')
+            self.calc_ok = False
+            return
 
         # fixed/calculation parameters
         i_plot = np.array(i_plot) - 1
@@ -5530,6 +5535,8 @@ class AnalysisGUI(QMainWindow):
 
                 # retrieves the cluster IDs/experiment names
                 i_expt_match = np.unique(r_obj_wc.i_expt[i_grp[0]])
+                j_ex = [list(r_obj_wc.i_expt0[i]).index(x) for x in i_expt_match]
+
                 cl_inc = [cfcn.get_inclusion_filt_indices(c, self.data.exc_gen_filt) for c in self.data._cluster]
                 clustID = [np.array(self.data._cluster[i_ex]['clustID'])[cl_inc[i_ex]] for i_ex in i_expt_match]
                 exp_name = [cf.extract_file_name(self.data.cluster[j]['expFile']) for j in i_expt_match]
@@ -5537,7 +5544,7 @@ class AnalysisGUI(QMainWindow):
                 for i_ex in range(n_ex):
                     # memory allocation and initialisations
                     v_sf_grp = [v_sf_sig_ex0[i][i_ex] for i in i_grp]
-                    clustIDex = np.array(clustID[i_ex])[r_obj_wc.clust_ind[i][i_ex]]
+                    clustIDex = np.array(clustID[i_ex])[r_obj_wc.clust_ind[i][j_ex[i_ex]]]
                     if len(clustIDex):
                         data_tmp[i_ex + 1] = np.zeros((len(clustIDex) + 1, n_col), dtype=object)
 
@@ -10713,7 +10720,7 @@ class AnalysisGUI(QMainWindow):
         # initialisations
         self.create_kinematic_lda_plots(self.data.discrim.spdacc, s_factor, marker_type, plot_grid, plot_chance=False)
 
-    def plot_speed_comp_lda(self, m_size, show_cell_sz, show_fit, sep_resp, plot_type, plot_grid):
+    def plot_speed_comp_lda(self, m_size, show_cell_sz, show_fit, sep_resp, plot_type, use_all, plot_grid):
         '''
 
         :param m_size:
@@ -10790,6 +10797,20 @@ class AnalysisGUI(QMainWindow):
             sz_cell = [m_size * (x / n_cell_mx) if show_cell_sz else m_size for x in n_cell_ex]
             y_acc_mn = 100. * np.mean(d_data.y_acc, axis=0)
 
+            if show_fit:
+                # sets the indices for fitting the curves
+                n_xi = len(d_data.spd_xi)
+                if use_all:
+                    i_fit = np.arange(n_xi).astype(int)
+                else:
+                    i_fit = np.arange(d_data.i_bin_spd + 1, n_xi).astype(int)
+
+                # calculates the psychometric curves
+                y_acc_mn, d_vel, vel_mx = np.mean(d_data.y_acc, axis=0), float(float(d_data.vel_bin)), 80.
+                xi_fit = np.arange(d_vel, vel_mx + 0.01, d_vel)
+                y_acc_fit, _, _, _ = cfcn.calc_psychometric_curves(y_acc_mn, xi_fit, n_cond, i_fit, d_data.i_bin_spd)
+
+
             ################################
             ####    SUBPLOT CREATION    ####
             ################################
@@ -10809,7 +10830,7 @@ class AnalysisGUI(QMainWindow):
 
                 # plots the psychometric fit (if required)
                 if show_fit:
-                    ax.plot(x_nw, 100. * d_data.y_acc_fit[:, i_cond], c=col[i_cond], linewidth=2)
+                    ax.plot(x_nw, 100. * y_acc_fit[:, i_cond], c=col[i_cond], linewidth=2)
 
                 # creates the legend
                 ax.legend(h_plt, d_data.ttype, loc=4)
@@ -10861,7 +10882,7 @@ class AnalysisGUI(QMainWindow):
         is_plot = np.array([x in plot_cond for x in d_data.ttype])
         n_cond, ttype = sum(is_plot), np.array(d_data.ttype)[is_plot]
 
-        # calculates the psychometric curves (if not present)
+        # calculates the psychometric curves
         cfcn.calc_all_psychometric_curves(d_data, float(d_data.vel_bin), use_all)
 
         #######################################
@@ -11036,7 +11057,7 @@ class AnalysisGUI(QMainWindow):
     ####    SINGLE EXPERIMENT ANALYSIS FUNCTIONS    ####
     ####################################################
 
-    def plot_signal_means(self, cell_id, plot_all, exp_name, plot_grid=True):
+    def plot_signal_means(self, cell_id, exp_name, plot_all, plot_grid):
         '''
 
         :param i_cluster:
@@ -11067,9 +11088,13 @@ class AnalysisGUI(QMainWindow):
         # sets up the figure/axis
         n_plot = len(i_cell)
         self.init_plot_axes(n_plot=n_plot)
+        n_col, n_row = cf.det_subplot_dim(n_plot)
 
         # plots the values over all subplots
         for i_plot in range(n_plot):
+            # sets the column/row indices
+            i_row, i_col = i_plot // n_col, i_plot % n_col
+            
             # plots the mean signal
             self.plot_fig.ax[i_plot].plot(T, data_plot['vMu'][:, i_cell[i_plot]], linewidth=3.0)
 
@@ -11077,15 +11102,21 @@ class AnalysisGUI(QMainWindow):
             self.plot_fig.ax[i_plot].grid(plot_grid)
             self.plot_fig.ax[i_plot].set_xlim(T[0], T[-1])
             self.plot_fig.ax[i_plot].set_title('Cluster #{0}'.format(c_id[i_plot]))
-            self.plot_fig.ax[i_plot].set_xlabel('Time (ms)')
-            self.plot_fig.ax[i_plot].set_ylabel('Voltage ({0}V)'.format(cf._mu))
+            
+            # sets the y-axis label (first column only)
+            if i_col == 0:
+                self.plot_fig.ax[i_plot].set_ylabel('Voltage ({0}V)'.format(cf._mu))
 
-    def plot_cluster_auto_ccgram(self, cell_id, plot_all, window_size, exp_name):
+            # sets the x-axis label (last row only)
+            if cf.is_final_row(i_row, i_col, n_row, n_col, n_plot):
+                self.plot_fig.ax[i_plot].set_xlabel('Time (ms)')
+
+    def plot_cluster_auto_ccgram(self, cell_id, exp_name, plot_all, window_size):
         '''
 
-        :param i_cluster:
-        :param plot_all:
-        :param c_type:
+        :param cell_id:
+        :param window_size:
+        :param exp_name:
         :return:
         '''
 
@@ -11107,12 +11138,13 @@ class AnalysisGUI(QMainWindow):
 
         # sets the indices of the clusters to plot and creates the figure/axis objects
         n_bin_tot = int(len(data_plot['ccGramXi']) / 2)
-        ind = np.arange(n_bin_tot - window_size, n_bin_tot + window_size)
+        ind = np.arange(n_bin_tot - window_size, n_bin_tot + (window_size + 1))
 
         # sets up the figure/axis
         n_plot = len(i_cell)
         self.init_plot_axes(n_plot=n_plot)
         xi_hist = data_plot['ccGramXi'][ind]
+        n_col, n_row = cf.det_subplot_dim(n_plot)
 
         # sets the x-axis limits
         bin_sz = xi_hist[1] - xi_hist[0]
@@ -11120,6 +11152,9 @@ class AnalysisGUI(QMainWindow):
 
         # plots the values over all subplots
         for i_plot in range(n_plot):
+            # sets the column/row indices
+            i_row, i_col = i_plot // n_col, i_plot % n_col
+
             # creates the bar-graph and the centre marker
             n_hist = data_plot['ccGram'][i_cell[i_plot], i_cell[i_plot], ind]
             self.plot_fig.ax[i_plot].bar(xi_hist, height=n_hist, width=bin_sz)
@@ -11131,9 +11166,15 @@ class AnalysisGUI(QMainWindow):
 
             # sets the axis properties
             self.plot_fig.ax[i_plot].set_title('Cluster #{0}'.format(c_id[i_plot]))
-            self.plot_fig.ax[i_plot].set_ylabel('Frequency (Hz)')
-            self.plot_fig.ax[i_plot].set_xlabel('Time Lag (ms)')
             self.plot_fig.ax[i_plot].set_xlim(x_lim)
+
+            # sets the y-axis label (first column only)
+            if i_col == 0:
+                self.plot_fig.ax[i_plot].set_ylabel('Frequency (Hz)')
+
+            # sets the x-axis label (last row only)
+            if cf.is_final_row(i_row, i_col, n_row, n_col, n_plot):
+                self.plot_fig.ax[i_plot].set_xlabel('Time Lag (ms)')
 
     def plot_cluster_cross_ccgram(self, exp_name, i_ref, i_comp, plot_all, m_size, plot_type,
                                   window_size, p_lim, f_cutoff):
@@ -11169,6 +11210,7 @@ class AnalysisGUI(QMainWindow):
 
         # sets up the figure/axis
         self.init_plot_axes(n_plot=n_plot)
+        n_col, n_row = cf.det_subplot_dim(n_plot)
 
         # sets the x-axis limits
         bin_sz = xi_hist[1] - xi_hist[0]
@@ -11183,6 +11225,9 @@ class AnalysisGUI(QMainWindow):
         for i_plot in range(n_plot):
             # sets the actual fixed/free plot indices
             search_lim = []
+
+            # sets the column/row indices
+            i_row, i_col = i_plot // n_col, i_plot % n_col
 
             # calculates the confidence intervals
             n_hist = data_plot['ccGram'][i_cell_ref, i_cell_comp[i_plot], ind]
@@ -11211,10 +11256,16 @@ class AnalysisGUI(QMainWindow):
 
             # sets the axis properties
             self.plot_fig.ax[i_plot].set_title('Cluster #{0} vs #{1}'.format(c_id_ref, c_id_comp[i_plot]))
-            self.plot_fig.ax[i_plot].set_ylabel('Frequency (Hz)')
-            self.plot_fig.ax[i_plot].set_xlabel('Time Lag (ms)')
             self.plot_fig.ax[i_plot].set_xlim(x_lim)
             self.plot_fig.ax[i_plot].set_ylim(y_lim)
+            
+            # sets the y-axis label (first column only)
+            if i_col == 0:
+                self.plot_fig.ax[i_plot].set_ylabel('Frequency (Hz)')
+
+            # sets the x-axis label (last row only)
+            if cf.is_final_row(i_row, i_col, n_row, n_col, n_plot):
+                self.plot_fig.ax[i_plot].set_xlabel('Time Lag (ms)')            
 
     #########################################
     ####    COMMON ANALYSIS FUNCTIONS    ####
@@ -18115,6 +18166,7 @@ class AnalysisFunctions(object):
                               ['show_cell_sz', 'Inter-Quartile Ranges'],
                               ['m_size', 'Inter-Quartile Ranges']]
             },
+            'use_all': {'type': 'B', 'text': 'Fit Curves Using All Points', 'def_val': True},
             'plot_grid': {'type': 'B', 'text': 'Show Axes Grid', 'def_val': False},
         }
         self.add_func(type='Speed LDA',
@@ -18255,7 +18307,7 @@ class AnalysisFunctions(object):
                 'type': 'L', 'text': 'Experiment', 'list': fix_exp, 'def_val': fix_exp[0],
                 'para_reset': [['cell_id', self.reset_fix_mult_cid]]
             },
-            'plot_all': {'type': 'B', 'text': 'Plot All Clusters', 'def_val': True, 'link_para':['cell_id', True]},
+            'plot_all': {'type': 'B', 'text': 'Plot All Clusters', 'def_val': True, 'link_para': ['cell_id', True]},
             'plot_grid': {'type': 'B', 'text': 'Show Axes Grid', 'def_val': False},
         }
         self.add_func(type='Single Experiment Analysis',
@@ -18275,7 +18327,7 @@ class AnalysisFunctions(object):
                 'type': 'L', 'text': 'Experiment', 'list': fix_exp, 'def_val': fix_exp[0],
                 'para_reset': [['cell_id', self.reset_fix_mult_cid]]
             },
-            'plot_all': {'type': 'B', 'text': 'Plot All Clusters', 'def_val': True, 'link_para':['cell_id', True]},
+            'plot_all': {'type': 'B', 'text': 'Plot All Clusters', 'def_val': True, 'link_para': ['cell_id', True]},
             'window_size': {'text': 'Window Size (ms)', 'def_val': 10, 'min_val': 5, 'max_val': 50},
         }
         self.add_func(type='Single Experiment Analysis',
@@ -18295,7 +18347,7 @@ class AnalysisFunctions(object):
                 'type': 'L', 'text': 'Experiment', 'def_val': fix_exp[0], 'list': fix_exp,
                 'para_reset': [['i_ref', self.reset_fix_cid], ['i_comp', self.reset_fix_mult_cid]]
             },
-            'plot_all': {'type': 'B', 'text': 'Plot All Clusters', 'def_val': True, 'link_para':['i_comp', True]},
+            'plot_all': {'type': 'B', 'text': 'Plot All Clusters', 'def_val': True, 'link_para': ['i_comp', True]},
             'm_size': {'text': 'Scatterplot Markersize', 'def_val': 30},
             'plot_type': {
                 'type': 'L', 'text': 'Plot Type', 'def_val': 'bar', 'list': ['bar', 'scatterplot'],
@@ -19256,10 +19308,10 @@ class AnalysisFunctions(object):
 
     def reset_fix_mult_cid(self, p_name, in_value):
         '''
-        
-        :param p_name: 
-        :param in_value: 
-        :return: 
+
+        :param p_name:
+        :param in_value:
+        :return:
         '''
 
         # retrieves the dropdown list object handles
@@ -19429,7 +19481,7 @@ class AnalysisFunctions(object):
             # otherwise, flag that there are no fixed/free cell matches
             nw_state = np.zeros(1, dtype=bool)
             nw_list = ['No Valid Fixed/Free Cells']
-            
+
             # resets the parameter value
             self.curr_para[p_name] = []
 

analysis_guis/rotational_analysis.py
@@ -920,7 +920,7 @@ def apply_single_rot_filter(data, d_clust, rot_filt, expt_filter_lvl, i_expt_mat
                             if i_expt_f2f is None:
                                 # determines the experiment/mapping indices for each of the freely moving data files
                                 if use_raw:
-                                    cl_ind_0 = [np.array(x['nC']) for x in d_clust]
+                                    cl_ind_0 = [np.arange(x['nC']) for x in d_clust]
                                 else:
                                     c_full = [data._cluster[x] for x in np.where(cf.det_valid_rotation_expt(data))[0]]
                                     cl_ind_0 = [np.where(cfcn.get_inclusion_filt_indices(c, g_filt))[0] for c in c_full]
@@ -994,13 +994,20 @@ def apply_single_rot_filter(data, d_clust, rot_filt, expt_filter_lvl, i_expt_mat
                         if i_expt_f2f is None:
                             # determines the experiment/mapping indices for each of the freely moving data files
                             if use_raw:
-                                cl_ind_0 = [np.array(x['nC']) for x in d_clust]
+                                cl_ind_0 = [np.arange(x['nC']) for x in d_clust]
                             else:
                                 c_full = [data._cluster[x] for x in np.where(cf.det_valid_rotation_expt(data))[0]]
                                 cl_ind_0 = [np.where(cfcn.get_inclusion_filt_indices(c, g_filt))[0] for c in c_full]
 
-                            i_expt_f2f, f2f_map = cf.det_matching_fix_free_cells(data, \
-                                                exp_name=data.externd.free_data.exp_name, cl_ind=cl_ind_0)
+
+                            # determines the free experiments matching the cluster experiment names
+                            fix_name_comp, ex_data = [x.fix_name for x in data.comp.data], data.externd
+                            free_name = [data.comp.data[fix_name_comp.index(x)].free_name for x in
+                                                       [cf.extract_file_name(x['expFile']) for x in d_clust]]
+                            free_exp = [cf.det_closest_file_match(ex_data.free_data.exp_name, x)[0] for x in free_name]
+
+                            i_expt_f2f, f2f_map = cf.det_matching_fix_free_cells(data,
+                                                                                 exp_name=free_exp, cl_ind=cl_ind_0)
 
                             # retrieves the fixed data files corresponding to the matches above
                             fix_name_f2f = [data.comp.data[x].fix_name for x in i_expt_f2f]