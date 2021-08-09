2914a1917c127f71e6ace8df2776de6d16b7a63b
Richard Faville <richard.faville@gmail.com>
2021-08-10 00:36:14 +0200

    Updated setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -9,19 +9,23 @@ curdir = op.dirname(op.realpath(__file__))
 readme = open(op.join(curdir, 'README.md')).read()
 
 requirements = [
-    "numpy <= 1.19.1",
-    "pycparser <= 2.20",
-    "pyopencl <= 2020.2",
-    "pyOpenSSL <= 19.1.0",
-    "PySocks <= 1.7.1",
-    "pytools <= 2020.3.1",
-    "pywin32 <= 227",
-    "requests <= 2.23.0",
-    "ruamel-yaml <= 0.15.87",
-    "six <= 1.14.0",
-    "urllib3 <= 1.25.8",
-    "win-inet-pton <= 1.1.0",
-    "wincertstore <= 0.2",
+    "mpldatacursor >= 0.6.2",
+    "numpy >= 1.18.4",
+    "pandas >= 0.25.3",
+    "PeakUtils >= 1.3.2",
+    "PyQt5 >= 5.11.3",
+    "PyQt5-sip >= 4.19.13",
+    "pyqt5-tools >= 5.11.2.1.3",
+    "rpy2 >= 3.3.3",
+    "scikit-image >= 0.16.2",
+    "scikit-learn >= 0.21.2",
+    "scikit-posthocs >= 0.6.2",
+    "scipy >= 1.5.2",
+    "seaborn >= 0.9.0",
+    "setuptools >= 39.0.1",
+    "shapely <= 1.7.1",
+    "tqdm >= 4.28.1",
+    "xlsxwriter >= 1.1.2",	
 ]
 
 setup(
@@ -39,67 +43,7 @@ setup(
         # ''               # vest_phys
         requirements,
     ],
-    url="https://github.com/RichardFav/AnalysisGUI",
+    url="https://github.com/RichardFav/spikeGUI",
     author="Richard Faville",
     author_email="richard.faville@gmail.com",
 )
-
-
-# from setuptools import setup, find_packages
-#
-# requirements = [
-#     "numpy",
-#     "scipy <= 1.2.1",
-#     "pandas",
-#     "matplotlib",
-#     "seaborn",
-#     "pycircstat",
-#     "nose",
-#     "decorator",
-#     "xlrd",
-# ]
-#
-#
-# setup(
-#     name="opendirection",
-#     version="0.1.0",
-#     description="Analysis of spiking activity in an open field",
-#     packages=find_packages(),
-#     include_package_data=True,
-#     install_requires=requirements,
-#     entry_points={
-#         "console_scripts": [
-#             "opendirection = opendirection.main:main",
-#             "opendirection_batch = opendirection.batch:main",
-#             "gen_velo_profile = opendirection.utils.generate_velo_profile:main",
-#         ]
-#     },
-#     url="https://github.com/adamltyson/opendirection",
-#     author="Adam Tyson",
-#     author_email="adam.tyson@ucl.ac.uk",
-# )
-
-
-# setup(
-#     name = 'MyProject',
-#     version = '0.1.0',
-#     url = '',
-#     description = '',
-#     packages = find_packages(),
-#     install_requires = [
-#         # Github Private Repository
-#         'ExampleRepo @ git+ssh://git@github.com/example_organization/ExampleRepo.git#egg=ExampleRepo-0.1'
-#     ]
-# )
-#
-# setup(
-#     name='<package>',
-# ...
-#     install_requires=[
-#         '<normal_dependency>',
-#          # Private repository
-#         '<dependency_name> @ git+ssh://git@github.com/<user>/<repo_name>@<branch>',
-#          # Public repository
-#         '<dependency_name> @ git+https://github.com/<user>/<repo_name>@<branch>',
-#     ],
-# )
