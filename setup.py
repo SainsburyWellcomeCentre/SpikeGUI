2df9dbf38f4f438ceb0e39c43882a5b87548d6ca
Richard Faville <richard.faville@gmail.com>
2021-08-10 22:44:51 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -25,17 +25,20 @@ requirements = [
     "setuptools >= 39.0.1",
     "shapely <= 1.7.1",
     "tqdm >= 4.28.1",
-    "xlsxwriter >= 1.1.2",	
+    "xlsxwriter >= 1.1.2",
+    "venn >= 0.1.3",
+    "matplotlib-venn >= 0.1.15",
+    "python-Levenshtein >= 0.12.0",
+    "cached-property >= 1.5.1",
+    "configobj >= 5.0.6",
 ]
 
 setup(
-    name='EPhyAnalysis',
+    name='spikeGUI',
     version='1.0',
     description='GUI-based program that analyses the spiking activity from Neuropixel recording experiment',
     long_description=readme,
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires = [
+    install_requires=[
         # ''               # margrie_libs
         # 'https://github.com/stephenlenzi/probez.git',      # probez repository
         # ''               # pyphys
@@ -43,7 +46,15 @@ setup(
         # ''               # vest_phys
         requirements,
     ],
-    url="https://github.com/RichardFav/spikeGUI",
+    python_requires=">=3.6",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "spikeGUI = analysis_guis.spike_gui:main",
+        ]
+    },
+    include_package_data=True,
     author="Richard Faville",
     author_email="richard.faville@gmail.com",
+    url="https://github.com/RichardFav/spikeGUI",
 )
