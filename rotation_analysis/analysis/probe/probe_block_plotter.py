29c4d2b605eb918d83148afa2b7b89ed7167a3c5
Richard Faville <richard.faville@gmail.com>
2021-08-10 23:08:09 +0200

    Delete rotation_analysis directory

--- rotation_analysis/analysis/probe/probe_block_plotter.py
+++
@@ -1,5 +0,0 @@
-from rotation_analysis.analysis.block_plotter import BlockPlotter
-
-
-class ProbeBlockPlotter(BlockPlotter):
-    pass
--- rotation_analysis/analysis/probe/probe_exceptions.py
+++
@@ -1,3 +0,0 @@
-class NoKeptTrialsError(Exception):
-    def __init__(self):
-        print('there are no trials to analyse, please check your condition entries and try again')
